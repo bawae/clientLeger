bcf203e005520e9d4a0da1b2198d9ee0abbf69ea
Wael Bayoudh <bayoudh.wael@polymtl.ca>
2016-02-01 15:57:26 -0500

    add project

---
+++ projet3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  projet3
+//
+//  Created by Adnane Chaabi on 2016-01-25.
+//  Copyright © 2016 Adnane Chaabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
