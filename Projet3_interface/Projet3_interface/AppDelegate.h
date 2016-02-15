d21de0ce675ce9b0564df77ff4471099b3755672
adncha <adnane.chaabi@myseat.fr>
2016-02-15 11:26:49 -0500

    add interface

---
+++ Projet3_interface/Projet3_interface/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Projet3_interface
+//
+//  Created by Adnane Chaabi on 2016-02-15.
+//  Copyright © 2016 Adnane Chaabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
